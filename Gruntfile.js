de29b1b357be2a27d4bf34205ebbd2c82cecfc2e
Arjun Puri <arjun.puri@publicissapient.com>
2021-06-25 10:16:40 +0530

    changed notes font. added minify commands for JS and CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = (grunt) => {
   const sass = require('sass');
   require('load-grunt-tasks')(grunt);
 
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.initConfig({
     sass: {
       options: {
@@ -14,7 +17,21 @@ module.exports = (grunt) => {
         },
       },
     },
+    cssmin: {
+      target: {
+        files: {
+          'src/components/note/note.min.css': 'src/components/note/note.css',
+        },
+      },
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'src/components/main.min.js': 'src/components/main.js',
+        },
+      },
+    },
   });
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
 };
--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,3 @@
-import Note from './note-2/note.js';
 import NotesContainer from './note-2-container/notes-container.js';
 import headerComponent from './header/header.js';
 import footerComponent from './footer/footer.js';
@@ -8,11 +7,5 @@ $(document).ready(() => {
   // rendering the Notes Container
   const notesContainer = new NotesContainer();
   notesContainer.render();
-
-  // rendering few notes
-  // const note1 = new Note('kappa');
-  // console.log(note1);
-  // note1.render();
-
   footerComponent.init('Arjun Puri');
 });
